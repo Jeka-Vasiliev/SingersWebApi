091ecc140b0d2bd636a2463b4e54ae6435542936
Jeka-Vasiliev <zheka_v@inbox.ru>
2017-05-16 01:30:38 +0300

    Add rest api

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,42 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace SingersWebApi.Controllers
 {
-    [Route("api/[controller]")]
-    public class ValuesController : Controller
+    [Route("[controller]")]
+    public class ListsController : Controller
     {
-        // GET api/values
+        private readonly ListsContext _context;
+
+        public ListsController(ListsContext context)
+        {
+            _context = context;
+        }
+
+        // GET lists
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IEnumerable<Model.List>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return await _context.Lists
+                .AsNoTracking()
+                .ToArrayAsync();
         }
 
-        // GET api/values/5
+        // GET lists/1
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Task<Model.List> Get(int id)
         {
-            return "value";
+            return _context.Lists
+                .FindAsync(id);
         }
 
-        // POST api/values
+        // POST lists
         [HttpPost]
-        public void Post([FromBody]string value)
+        public async Task Post([FromBody]Model.List value)
         {
+            value.Created = DateTime.Now;
+            value.Updated = DateTime.Now;
+            await _context.AddAsync(value);
+            await _context.SaveChangesAsync();
         }
 
-        // PUT api/values/5
+        // PUT lists/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public async Task Put(int id, [FromBody]Model.List value)
         {
+            var editedModel = await _context.Lists.FirstAsync(l => l.Id == id);
+            editedModel.Name = value.Name;
+            editedModel.Order = value.Order;
+            editedModel.FirstName = value.FirstName;
+            editedModel.LastName = value.LastName;
+            editedModel.BirthDate = value.BirthDate;
+            editedModel.IsAlive = value.IsAlive;
+            editedModel.Updated = DateTime.Now;
+            await _context.SaveChangesAsync();
         }
 
-        // DELETE api/values/5
+        // DELETE lists/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task Delete(int id)
         {
+            var deletedModel = await _context.Lists.FirstAsync(l => l.Id == id);
+            _context.Lists.Remove(deletedModel);
+            await _context.SaveChangesAsync();
         }
     }
 }
