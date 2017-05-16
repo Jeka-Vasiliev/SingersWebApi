1ec11e94d2d89f8a2507e2c3d64b7590110a483e
Jeka-Vasiliev <zheka_v@inbox.ru>
2017-05-16 09:32:05 +0300

    Add viewmodel for POST, PATCH and PUT

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using SingersWebApi.ViewModels;
 
 namespace SingersWebApi.Controllers
 {
@@ -36,25 +37,40 @@ namespace SingersWebApi.Controllers
 
         // POST lists
         [HttpPost]
-        public async Task Post([FromBody]Model.List value)
+        public async Task Post([FromBody]PostListViewModel value)
         {
-            value.Created = DateTime.Now;
-            value.Updated = DateTime.Now;
+            value.List.Created = DateTime.Now;
+            value.List.Updated = DateTime.Now;
             await _context.AddAsync(value);
             await _context.SaveChangesAsync();
         }
 
         // PUT lists/5
         [HttpPut("{id}")]
-        public async Task Put(int id, [FromBody]Model.List value)
+        public async Task Put(int id, [FromBody]PutListViewModel value)
         {
             var editedModel = await _context.Lists.FirstAsync(l => l.Id == id);
-            editedModel.Name = value.Name;
-            editedModel.Order = value.Order;
-            editedModel.FirstName = value.FirstName;
-            editedModel.LastName = value.LastName;
-            editedModel.BirthDate = value.BirthDate;
-            editedModel.IsAlive = value.IsAlive;
+            editedModel.Name = value.List.Name;
+            editedModel.Order = value.List.Order;
+            editedModel.FirstName = value.List.FirstName;
+            editedModel.LastName = value.List.LastName;
+            editedModel.BirthDate = value.List.BirthDate;
+            editedModel.IsAlive = value.List.IsAlive;
+            editedModel.Updated = DateTime.Now;
+            await _context.SaveChangesAsync();
+        }
+
+        // PUT lists/5
+        [HttpPatch("{id}")]
+        public async Task Patch(int id, [FromBody]PatchListViewModel value)
+        {
+            var editedModel = await _context.Lists.FirstAsync(l => l.Id == id);
+            editedModel.Name = value.List.Name;
+            editedModel.Order = value.List.Order;
+            editedModel.FirstName = value.List.FirstName;
+            editedModel.LastName = value.List.LastName;
+            editedModel.BirthDate = value.List.BirthDate;
+            editedModel.IsAlive = value.List.IsAlive;
             editedModel.Updated = DateTime.Now;
             await _context.SaveChangesAsync();
         }
---
+++ ViewModels/PatchListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace SingersWebApi.ViewModels
+{
+    [DataContract]
+    public class PatchListViewModel
+    {
+        [DataMember(Name = "list")]
+        public Model.List List { get; set; }
+    }
+}
